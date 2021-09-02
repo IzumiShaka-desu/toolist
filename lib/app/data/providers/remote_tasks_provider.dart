22d079db81a92795b873e37849d07f51e3a882a1
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-02 11:50:38 +0700

    ✨ add: method "add" on RemoteTaskaProvider

--- lib/app/data/providers/remote_tasks_provider.dart
+++ lib/app/data/providers/remote_tasks_provider.dart
@@ -1,9 +1,29 @@
 import 'package:get/get.dart';
 import 'package:supabase/supabase.dart';
+import 'package:toolist/app/core/utils/debug_utils.dart';
 
 import '../models/tasks_model.dart';
 
 class RemoteTasksProvider {
   static const _tableName = 'tasks';
   final SupabaseClient _client = Get.find();
+
+  Future<bool> add(Tasks newTask) async {
+    final res = await _client
+        .from(
+          _tableName,
+        )
+        .upsert(
+          newTask,
+        )
+        .execute();
+    if (res.error != null) {
+      DebugUtils.print(
+        className: 'RemoteTaskProvider',
+        message: res.error!.message,
+      );
+      return false;
+    }
+    return true;
+  }
 }
