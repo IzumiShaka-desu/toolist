390ba8c0baf739cc7715a7d3098098bc36e4b15a
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-02 19:29:17 +0700

    🚨 refactor: fix linter warning

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:toolist/app/core/values/color_palette.dart';
+import 'package:toolist/app/modules/widgets/avatar_image.dart';
+import 'package:toolist/app/modules/widgets/home_container.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,16 +20,18 @@ class HomeView extends GetView<HomeController> {
               Container(
                 color: ColorPalette.mainBlue,
               ),
+              // ignore: sized_box_for_whitespace
               Container(
                 width: Get.width * 0.5,
                 child: SizedBox(
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      SizedBox(height: 15),
+                      const SizedBox(height: 15),
+                      // ignore: sized_box_for_whitespace
                       Container(
                         child: Container(
-                          padding: EdgeInsets.all(5),
+                          padding: const EdgeInsets.all(5),
                           decoration: BoxDecoration(
                             shape: BoxShape.circle,
                             border: Border.all(
@@ -37,9 +43,9 @@ class HomeView extends GetView<HomeController> {
                         height: 75,
                         width: Get.width * 0.5,
                       ),
-                      VerticalDivider(thickness: 2),
+                      const VerticalDivider(thickness: 2),
                       Container(
-                        padding: EdgeInsets.only(top: 20, left: 15),
+                        padding: const EdgeInsets.only(top: 20, left: 15),
                         child: Text(
                           // provider.currentUser.name,
                           'test',
@@ -49,7 +55,7 @@ class HomeView extends GetView<HomeController> {
                         ),
                       ),
                       ListTile(
-                        leading: Icon(
+                        leading: const Icon(
                           Icons.home_outlined,
                           color: ColorPalette.mainWhite,
                         ),
@@ -69,7 +75,7 @@ class HomeView extends GetView<HomeController> {
                           controller.closeMenu();
                           controller.tabController.animateTo(1);
                         },
-                        leading: Icon(
+                        leading: const Icon(
                           Icons.account_circle,
                           color: ColorPalette.mainWhite,
                         ),
@@ -83,7 +89,7 @@ class HomeView extends GetView<HomeController> {
                       ListTile(
                         onTap: controller.logout,
                         hoverColor: ColorPalette.mainBlue.withOpacity(0.9),
-                        leading: Icon(
+                        leading: const Icon(
                           Icons.exit_to_app_outlined,
                           color: ColorPalette.mainWhite,
                         ),
@@ -108,7 +114,7 @@ class HomeView extends GetView<HomeController> {
                           color: ColorPalette.mainWhite.withOpacity(0.5)),
                     ),
                     child: IconButton(
-                      icon: Icon(Icons.chevron_left_outlined),
+                      icon: const Icon(Icons.chevron_left_outlined),
                       iconSize: 32,
                       onPressed: controller.closeMenu,
                       color: ColorPalette.mainWhite,
@@ -117,122 +123,13 @@ class HomeView extends GetView<HomeController> {
               AnimatedPositioned(
                 top: controller.isMenuOpened ? Get.height * 0.08 : 0,
                 left: controller.isMenuOpened ? Get.width * 0.6 : 0,
-                duration: Duration(milliseconds: 250),
+                duration: const Duration(milliseconds: 250),
                 child: const HomeContainer(),
               ),
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class HomeContainer extends GetView<HomeController> {
-  const HomeContainer({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: Get.height,
-      width: Get.width,
-      child: Obx(
-        () => Transform.scale(
-          alignment: Alignment.topRight,
-          scale: controller.isMenuOpened ? 0.85 : 1,
-          child: AnimatedContainer(
-            duration: Duration(
-              milliseconds: 300,
-            ),
-            padding: EdgeInsets.all(
-              controller.isMenuOpened ? 25 : 0,
-            ),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(
-                  20,
-                ),
-                color: ColorPalette.secondary),
-            child: Scaffold(
-              backgroundColor: ColorPalette.secondary,
-              appBar: AppBar(
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                leading: const OpenMenuButton(),
-                actions: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.search,
-                      color: Colors.grey,
-                    ),
-                    onPressed: () {},
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.notifications_none_outlined,
-                      color: Colors.grey,
-                    ),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-              body: const TabBarViewHome(),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TabBarViewHome extends GetView<HomeController> {
-  const TabBarViewHome({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TabBarView(
-      physics: NeverScrollableScrollPhysics(),
-      controller: controller.tabController,
-      children: controller.pages,
-    );
-  }
-}
-
-class OpenMenuButton extends GetView<HomeController> {
-  const OpenMenuButton({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.menu_rounded,
-        color: Colors.grey,
-      ),
-      onPressed: () {
-        controller.isMenuOpened
-            ? controller.closeMenu()
-            : controller.openMenu();
-      },
-    );
-  }
-}
-
-class AvatarImage extends GetView<HomeController> {
-  const AvatarImage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const CircleAvatar(
-      minRadius: 35,
-      maxRadius: 40,
-      backgroundColor: ColorPalette.mainWhite,
     );
   }
 }
