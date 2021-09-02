6c731ef8bf0aa655e9411a9a933408b0c25b79fd
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-02 12:44:39 +0700

    ✨ add: initialization tasklist

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,9 +1,35 @@
 import 'package:get/get.dart';
+import 'package:toolist/app/core/utils/debug_utils.dart';
+import 'package:toolist/app/data/models/tasks_model.dart';
+import 'package:toolist/app/data/repository/tasks_repository.dart';
 
 class HomeController extends GetxController {
+  final TasksRepository _repository = Get.find();
+  final _taskList = <Tasks>[].obs;
+  List<Tasks> get taskList => _taskList;
+  final _isLoading = false.obs;
+  bool get isLoading => _isLoading.value;
   @override
   void onInit() {
+    Future.wait(
+      [
+        loadTasks(),
+      ],
+    );
     super.onInit();
+  }
+
+  Future<void> loadTasks() async {
+    _isLoading.value = true;
+    try {
+      _taskList.value = await _repository.getList();
+    } catch (e) {
+      DebugUtils.print(
+        className: 'LoadTask',
+        message: e.toString(),
+      );
+    }
+    _isLoading.value = false;
   }
 
   // @override
