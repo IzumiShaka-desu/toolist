4f71f0d29b8955781c9113254ffef7b9de650f4c
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-02 19:22:01 +0700

    🎨 changes: separate widget

---
+++ lib/app/modules/widgets/tabbarview_home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:toolist/app/modules/home/controllers/home_controller.dart';
+
+class TabBarViewHome extends GetView<HomeController> {
+  const TabBarViewHome({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBarView(
+      physics: NeverScrollableScrollPhysics(),
+      controller: controller.tabController,
+      children: controller.pages,
+    );
+  }
+}
