05e5fdd932baf4e6a7505fe5bbb4606cbeff5c9b
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-02 12:50:31 +0700

    📦 add: ColorPallete

---
+++ lib/app/core/values/color_palette.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorPalette {
+  static const mainBlue = Color.fromRGBO(13, 31, 83, 1);
+  static const secondaryBlue = Color.fromRGBO(6, 105, 255, 1);
+  static const altBlue = Color.fromRGBO(10, 105, 249, 1);
+  static const pink = Color.fromRGBO(235, 5, 254, 1);
+  static const textDark = Color.fromRGBO(30, 41, 82, 1);
+  static const mainWhite = Color.fromRGBO(253, 255, 255, 1);
+  static const secondary = Color.fromRGBO(248, 250, 255, 1);
+  static const primaryBlue = Color.fromRGBO(9, 20, 87, 1);
+  static const secondaryBLue = Color.fromRGBO(64, 69, 126, 1);
+  static const primaryGreen = Color.fromRGBO(52, 177, 127, 1);
+  static const secondaryGreen = Color.fromRGBO(104, 202, 165, 1);
+}
