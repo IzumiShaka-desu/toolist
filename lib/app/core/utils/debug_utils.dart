54f822b0bbd36b0740a75ece5737dba56dec9ba6
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-02 11:49:15 +0700

    ✨ add: print on DebugUtils

--- lib/app/core/utils/debug_utils.dart
+++ lib/app/core/utils/debug_utils.dart
@@ -1,4 +1,33 @@
-abstract class DebugUtils {}
+import 'dart:developer';
+
+abstract class DebugUtils {
+  static void print({
+    required String className,
+    required String message,
+    Priority priority = Priority.low,
+    Object? errorObject,
+    StackTrace? trace,
+  }) =>
+      log(
+        '$className: $message',
+        level: priorityToIntLevel(priority),
+        time: DateTime.now(),
+        error: errorObject,
+        name: className,
+        stackTrace: trace,
+      );
+
+  static int priorityToIntLevel(Priority priority) {
+    switch (priority) {
+      case Priority.high:
+        return 2;
+      case Priority.medium:
+        return 1;
+      case Priority.low:
+        return 0;
+    }
+  }
+}
 
 enum Priority {
   high,
