9cd915af3084c220dd91fe33b32d82ef9eab87ca
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-02 19:19:27 +0700

    🎨 changes: separate widget

---
+++ lib/app/modules/widgets/open_menu_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:toolist/app/modules/home/controllers/home_controller.dart';
+
+class OpenMenuButton extends GetView<HomeController> {
+  const OpenMenuButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        Icons.menu_rounded,
+        color: Colors.grey,
+      ),
+      onPressed: () {
+        controller.isMenuOpened
+            ? controller.closeMenu()
+            : controller.openMenu();
+      },
+    );
+  }
+}
