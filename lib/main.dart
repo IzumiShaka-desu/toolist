91191f74cda6b5b64c551de643a3593f61fc9488
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-02 10:24:05 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  await dotenv.load();
   runApp(
     GetMaterialApp(
       title: 'Toolist',
