2b7fbf8e23102b7f039449b9b2a0e7d50ffc6f73
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-02 14:52:11 +0700

    📦 update: add tabcontroller on homecontroller

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,16 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:toolist/app/core/utils/debug_utils.dart';
 import 'package:toolist/app/data/models/tasks_model.dart';
 import 'package:toolist/app/data/repository/tasks_repository.dart';
 
-class HomeController extends GetxController {
+class HomeController extends GetxController with SingleGetTickerProviderMixin {
   final TasksRepository _repository = Get.find();
+
   final _taskList = <Tasks>[].obs;
   List<Tasks> get taskList => _taskList;
+
   final _isLoading = false.obs;
   bool get isLoading => _isLoading.value;
+
+  late final TabController _controller;
+  TabController get tabController => _controller;
+
+  final _pages = [Container(), Container()];
+  List<Widget> get pages => _pages;
+
   @override
   void onInit() {
+    _controller = TabController(
+      length: pages.length,
+      vsync: this,
+    );
     Future.wait(
       [
         loadTasks(),
