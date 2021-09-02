8460f729e35932d924e9c780972bc52d470a232c
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-02 12:19:29 +0700

    ✨ add: add method TasksRepository

--- lib/app/data/repository/tasks_repository.dart
+++ lib/app/data/repository/tasks_repository.dart
@@ -1,1 +1,10 @@
-class TasksRepository {}
+import 'package:get/get.dart';
+import 'package:toolist/app/data/models/tasks_model.dart';
+import 'package:toolist/app/data/providers/remote_tasks_provider.dart';
+
+class TasksRepository {
+  final RemoteTasksProvider _remoteTasksProvider = Get.find();
+  Future<bool> add(Tasks newTask) async => _remoteTasksProvider.add(
+        newTask,
+      );
+}
