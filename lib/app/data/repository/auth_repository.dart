1cc20534a83f434e0f9c5964f565ef6a340be419
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-03 18:36:09 +0700

    ✨ add: register on authrepository

--- lib/app/data/repository/auth_repository.dart
+++ lib/app/data/repository/auth_repository.dart
@@ -1,1 +1,42 @@
-class AuthRepository {}
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:toolist/app/core/values/box_keys.dart';
+import 'package:toolist/app/data/models/response_model.dart';
+import 'package:toolist/app/data/providers/auth_provider.dart';
+import 'package:supabase/supabase.dart';
+
+class AuthRepository {
+  final AuthProvider _authProvider = Get.find();
+  final GoTrueClient authClient = Get.find<SupabaseClient>().auth;
+  final GetStorage _box = Get.find();
+  Future<ResponseModel> register({
+    required String email,
+    required String password,
+    required String fullname,
+  }) async {
+    final _result = await _authProvider.register(
+      email: email,
+      password: password,
+    );
+    return _result.fold(
+      (l) => ResponseModel(
+        message: l,
+        result: false,
+      ),
+      (r) {
+        _box.write(
+          BoxKeys.token,
+          r.data?.persistSessionString,
+        );
+        _box.write(
+          BoxKeys.savedFName,
+          fullname,
+        );
+        return ResponseModel(
+          message: 'register success',
+          result: true,
+        );
+      },
+    );
+  }
+}
