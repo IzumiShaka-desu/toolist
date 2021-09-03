277c65baddd81664dc931a95464edc58dfdbb41c
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-03 08:50:39 +0700

    ✨ add: register on authprovider

--- lib/app/data/providers/auth_provider.dart
+++ lib/app/data/providers/auth_provider.dart
@@ -1,1 +1,24 @@
-class AuthProvider {}
+import 'package:dartz/dartz.dart';
+import 'package:get/get.dart';
+import 'package:supabase/supabase.dart';
+
+class AuthProvider {
+  final SupabaseClient _client = Get.find();
+
+  Future<Either<String, GotrueSessionResponse>> register({
+    required String email,
+    required String password,
+  }) async {
+    final _result = await _client.auth.signUp(
+      email,
+      password,
+    );
+    if (_result.error != null) {
+      return Left(
+        _result.error!.message,
+      );
+    }
+
+    return Right(_result);
+  }
+}
