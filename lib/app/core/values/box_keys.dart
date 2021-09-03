46ff60aa216c6142770af0693e016688f2f60373
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-03 18:35:30 +0700

    📦 add: boxkeys on values

---
+++ lib/app/core/values/box_keys.dart
@@ -0,0 +1,6 @@
+abstract class BoxKeys {
+  static const token = 'TOKEN';
+  static const isSeen = 'isSeen';
+  static const savedFName = 'fullname';
+  static const profileImage = 'profile_image';
+}
