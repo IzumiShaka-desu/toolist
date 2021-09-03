adeb0eff0730682f6abd5a66459eee175c3ba9b9
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-03 08:43:31 +0700

    📦 add: auth module

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 
+import 'package:toolist/app/modules/auth/bindings/auth_binding.dart';
+import 'package:toolist/app/modules/auth/views/auth_view.dart';
 import 'package:toolist/app/modules/home/bindings/home_binding.dart';
 import 'package:toolist/app/modules/home/views/home_view.dart';
 
@@ -15,6 +17,11 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.AUTH,
+      page: () => AuthView(),
+      binding: AuthBinding(),
     ),
   ];
 }
