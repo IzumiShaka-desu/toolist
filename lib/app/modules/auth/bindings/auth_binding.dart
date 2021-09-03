015bf4dbc538b381d3915d46b65fd91627105ac9
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-03 18:43:26 +0700

    🏗️ configure: authbinding

--- lib/app/modules/auth/bindings/auth_binding.dart
+++ lib/app/modules/auth/bindings/auth_binding.dart
@@ -1,10 +1,20 @@
 import 'package:get/get.dart';
+import 'package:toolist/app/data/providers/auth_provider.dart';
+import 'package:toolist/app/data/repository/auth_repository.dart';
 
 import '../controllers/auth_controller.dart';
 
 class AuthBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut(
+      () => AuthProvider(),
+    );
+
+    Get.lazyPut(
+      () => AuthRepository(),
+    );
+
     Get.lazyPut<AuthController>(
       () => AuthController(),
     );
