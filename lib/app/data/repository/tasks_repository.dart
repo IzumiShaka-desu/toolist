6ab33b23964ed41f3de5332deedbed154c6bee87
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-03 09:51:35 +0700

    🎨 refactor: tasksprovder code structure

--- lib/app/data/repository/tasks_repository.dart
+++ lib/app/data/repository/tasks_repository.dart
@@ -4,14 +4,27 @@ import 'package:toolist/app/data/providers/remote_tasks_provider.dart';
 
 class TasksRepository {
   final RemoteTasksProvider _remoteTasksProvider = Get.find();
-  Future<bool> add(Tasks newTask) async => _remoteTasksProvider.add(
+
+  Future<bool> add(
+    Tasks newTask,
+  ) async =>
+      _remoteTasksProvider.add(
         newTask,
       );
-  Future<bool> update(Tasks newTask) async => _remoteTasksProvider.update(
+
+  Future<bool> update(
+    Tasks newTask,
+  ) async =>
+      _remoteTasksProvider.update(
         newTask,
       );
-  Future<bool> delete(String idTask) async => _remoteTasksProvider.delete(
+
+  Future<bool> delete(
+    String idTask,
+  ) async =>
+      _remoteTasksProvider.delete(
         idTask,
       );
+
   Future<List<Tasks>> getList() async => _remoteTasksProvider.get();
 }
