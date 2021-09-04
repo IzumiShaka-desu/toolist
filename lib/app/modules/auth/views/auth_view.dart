8acdb35b352ab648f03220ecb7f22360f8003fa7
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-04 09:52:03 +0700

    🚨 change: fix linter warning

--- lib/app/modules/auth/views/auth_view.dart
+++ lib/app/modules/auth/views/auth_view.dart
@@ -5,19 +5,10 @@ import 'package:get/get.dart';
 import '../controllers/auth_controller.dart';
 
 class AuthView extends GetView<AuthController> {
+  const AuthView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('AuthView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'AuthView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+    return const Scaffold();
   }
 }
