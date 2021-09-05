ecae968551addc06403b9dabc3f4f4d45f61bbe1
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-05 12:15:33 +0700

    ✨ add: ui mapping

--- lib/app/modules/home/views/pages/home_page.dart
+++ lib/app/modules/home/views/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:toolist/app/core/values/box_keys.dart';
+import 'package:toolist/app/core/values/color_palette.dart';
 import 'package:toolist/app/modules/home/controllers/home_controller.dart';
 
 class HomePage extends GetView<HomeController> {
@@ -7,6 +9,53 @@ class HomePage extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorPalette.secondary,
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(8),
+              child: Text(
+                'What\'s up, ${controller.user?.userMetadata[BoxKeys.savedFName]}! ',
+                style: Get.textTheme.headline5?.copyWith(
+                  fontWeight: FontWeight.w600,
+                  color: ColorPalette.mainBlue,
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              child: Row(
+                children: const [
+                  Expanded(
+                    child: Card(),
+                  ),
+                  Expanded(
+                    child: Card(),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              child: const Card(),
+            ),
+            SizedBox(
+              height: 10,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: controller.addTask,
+          backgroundColor: ColorPalette.altBlue,
+        ),
+      ),
+    );
   }
 }
