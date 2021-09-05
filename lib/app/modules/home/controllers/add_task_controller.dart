53e6c86ceb8059f563c222ac9e755bea03a0602d
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-05 14:33:12 +0700

    add texteditingcontroller

--- lib/app/modules/home/controllers/add_task_controller.dart
+++ lib/app/modules/home/controllers/add_task_controller.dart
@@ -1,3 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class AddtaskController extends GetxController {}
+class AddtaskController extends GetxController {
+  late final TextEditingController textEditingController;
+
+  @override
+  void onInit() {
+    textEditingController = TextEditingController();
+    super.onInit();
+  }
+
+  @override
+  void onClose() {
+    textEditingController.dispose();
+    super.onClose();
+  }
+}
