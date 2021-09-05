b9da98e3e09e16628f2b0206bfd97e404419b61a
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-05 14:43:14 +0700

    📦 add: add task view

---
+++ lib/app/modules/home/views/add_task_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:toolist/app/modules/home/controllers/add_task_controller.dart';
+
+class AddtaskView extends GetView<AddtaskController> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
