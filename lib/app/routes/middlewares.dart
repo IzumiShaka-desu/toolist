c49a99a103b47f94d787973bbca1d66086c4f795
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-05 09:09:39 +0700

    📦 add: not authorized middleware

--- lib/app/routes/middlewares.dart
+++ lib/app/routes/middlewares.dart
@@ -26,3 +26,22 @@ class AuthorizedMiddleware extends GetMiddleware {
     _auth.updateDataWhenFullnameAvailable();
   }
 }
+
+class NotAuthorizedMiddleware extends GetMiddleware {
+  NotAuthorizedMiddleware() : super(priority: 1);
+
+  @override
+  RouteSettings? redirect(String? route) {
+    final _isTokenAvailable = box.hasData(BoxKeys.token);
+    final _auth = _client.auth;
+    if (_isTokenAvailable) {
+      _auth.recoverSession(box.read(BoxKeys.token));
+      if (_auth.isSessionSet) {
+        _auth.updateDataWhenFullnameAvailable();
+        return const RouteSettings(
+          name: _Paths.HOME,
+        );
+      }
+    }
+  }
+}
