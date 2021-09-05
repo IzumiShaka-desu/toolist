25c05d19dbfcf4003479551c3377e431e7ee92fd
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-05 12:24:44 +0700

    ✨ add: tasklistview

--- lib/app/modules/home/views/pages/home_page.dart
+++ lib/app/modules/home/views/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:toolist/app/core/values/box_keys.dart';
 import 'package:toolist/app/core/values/color_palette.dart';
+import 'package:toolist/app/data/models/tasks_model.dart';
+import 'package:toolist/app/global_widget/organisms/pull_widget.dart';
 import 'package:toolist/app/modules/home/controllers/home_controller.dart';
 
 class HomePage extends GetView<HomeController> {
@@ -40,7 +42,7 @@ class HomePage extends GetView<HomeController> {
               ),
             ),
             Flexible(
-              child: const Card(),
+              child: TasksListView(),
             ),
             SizedBox(
               height: 10,
@@ -55,6 +57,165 @@ class HomePage extends GetView<HomeController> {
           onPressed: controller.addTask,
           backgroundColor: ColorPalette.altBlue,
         ),
+      ),
+    );
+  }
+}
+
+class TasksListView extends GetView<HomeController> {
+  const TasksListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PullWidget(
+        onLoad: (refreshController) {
+          refreshController.loadComplete();
+          // controller.loadTask().whenComplete(() =>
+          //  null);
+        },
+        onRefresh: (refreshController) {
+          controller.loadTasks().whenComplete(
+                () => refreshController.refreshCompleted(),
+              );
+        },
+        child: SingleChildScrollView(
+          child: Obx(
+            () => controller.isLoading
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : AnimatedList(
+                    shrinkWrap: true,
+                    reverse: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    padding: EdgeInsets.all(8),
+                    key: controller.animateKey,
+                    initialItemCount: controller.taskList.length,
+                    itemBuilder: (BuildContext context, int index, anim) {
+                      Tasks item = controller.taskList.elementAt(index);
+                      bool isPersonal = item.type == TaskType.personal;
+                      return ItemCard(
+                        anim: anim,
+                        item: item,
+                        index: index,
+                        isPersonal: isPersonal,
+                        onClickRemove: () => controller.removeTask(index),
+                      );
+                    },
+                  ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  const ItemCard({
+    Key? key,
+    required this.anim,
+    required this.index,
+    required this.item,
+    required this.isPersonal,
+    required this.onClickRemove,
+  }) : super(key: key);
+
+  final Animation<double> anim;
+  final int index;
+  final Tasks item;
+  final bool isPersonal;
+  final void Function() onClickRemove;
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: anim,
+      child: TaskContainer(
+        item: item,
+        index: index,
+        isPersonal: isPersonal,
+        trailing: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: onClickRemove,
+        ),
+      ),
+    );
+  }
+}
+
+class TaskContainer extends GetView<HomeController> {
+  const TaskContainer({
+    Key? key,
+    required this.item,
+    required this.isPersonal,
+    required this.index,
+    this.trailing,
+  }) : super(key: key);
+
+  final Tasks item;
+  final int index;
+  final bool isPersonal;
+  final Widget? trailing;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: InkWell(
+          onTap: () => controller.updateState(index),
+          child: CircleCheckBox(
+            isChecked: item.isDone ?? false,
+            color: isPersonal ? ColorPalette.altBlue : ColorPalette.pink,
+          ),
+        ),
+        title: Text(
+          item.taskName ?? '',
+          style: Get.textTheme.bodyText1?.copyWith(
+              fontWeight: FontWeight.w500,
+              decoration: item.isDone ?? false
+                  ? TextDecoration.lineThrough
+                  : TextDecoration.none,
+              fontSize: 17),
+        ),
+        trailing: trailing ?? SizedBox(),
+      ),
+      color: ColorPalette.mainWhite,
+    );
+  }
+}
+
+class CircleCheckBox extends StatelessWidget {
+  final bool isChecked;
+  final Color color;
+
+  const CircleCheckBox({
+    Key? key,
+    this.isChecked = false,
+    this.color = ColorPalette.altBlue,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: Duration(milliseconds: 250),
+      child: Container(
+        decoration: BoxDecoration(
+          color: isChecked ? color.withOpacity(0.5) : ColorPalette.mainWhite,
+          border: Border.all(
+            color: isChecked ? color.withOpacity(0.1) : color,
+            width: 2,
+          ),
+          shape: BoxShape.circle,
+        ),
+        child: isChecked
+            ? Icon(
+                Icons.check,
+                color: ColorPalette.mainWhite,
+              )
+            : Icon(
+                Icons.circle,
+                color: Colors.transparent,
+              ),
       ),
     );
   }
