cd7c465a9767f9d42659b4759a4187b302454759
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-05 12:48:06 +0700

    ⚡️ remove: unused import

--- lib/app/modules/home/views/pages/home_page.dart
+++ lib/app/modules/home/views/pages/home_page.dart
@@ -2,16 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:toolist/app/core/values/box_keys.dart';
 import 'package:toolist/app/core/values/color_palette.dart';
-import 'package:toolist/app/data/models/tasks_model.dart';
-import 'package:toolist/app/global_widget/organisms/pull_widget.dart';
 import 'package:toolist/app/modules/home/controllers/home_controller.dart';
 import 'package:toolist/app/modules/home/views/widgets/business_card.dart';
-import 'package:toolist/app/modules/home/views/widgets/circle_check_box.dart';
-import 'package:toolist/app/modules/home/views/widgets/count_text_view.dart';
-import 'package:toolist/app/modules/home/views/widgets/item_card.dart';
 import 'package:toolist/app/modules/home/views/widgets/personal_card.dart';
-import 'package:toolist/app/modules/home/views/widgets/task_container.dart';
-import 'package:toolist/app/modules/home/views/widgets/task_progress.dart';
 import 'package:toolist/app/modules/home/views/widgets/tasks_list_view.dart';
 
 class HomePage extends GetView<HomeController> {
