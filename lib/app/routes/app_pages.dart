29534957de5f2b753a287604a4ce842866c9f38a
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-05 09:26:07 +0700

    🏗️ register middleware

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:supabase/supabase.dart';
+import 'package:toolist/app/core/utils/auth_utils.dart';
+import 'package:toolist/app/core/values/box_keys.dart';
 
 import 'package:toolist/app/modules/auth/bindings/auth_binding.dart';
 import 'package:toolist/app/modules/auth/views/auth_view.dart';
@@ -16,13 +21,19 @@ class AppPages {
   static final routes = [
     GetPage(
       name: _Paths.HOME,
-      page: () => HomeView(),
+      page: () => const HomeView(),
       binding: HomeBinding(),
+      middlewares: [
+        AuthorizedMiddleware(),
+      ],
     ),
     GetPage(
       name: _Paths.AUTH,
-      page: () => AuthView(),
+      page: () => const AuthView(),
       binding: AuthBinding(),
+      middlewares: [
+        NotAuthorizedMiddleware(),
+      ],
     ),
   ];
 }
