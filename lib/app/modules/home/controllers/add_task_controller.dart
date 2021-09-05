092a5a717709dc24271e9dbf2b15b3ba2f75da08
IzumiShaka-desu <shakaaji29@gmail.com>
2021-09-05 14:38:57 +0700

    ✨ add: date property

--- lib/app/modules/home/controllers/add_task_controller.dart
+++ lib/app/modules/home/controllers/add_task_controller.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:toolist/app/core/utils/datetime_utils.dart';
 
 class AddtaskController extends GetxController {
   late final TextEditingController textEditingController;
+
+  final _date = DateTime.now().obs;
+
+  DateTime get date => _date.value;
+
+  set dateTime(DateTime newDate) => _date.value = newDate;
+
+  bool get isToday => _date.value.isToday;
+
+  bool get isTomorrow => _date.value.isTomorrow;
 
   @override
   void onInit() {
